a853b83c852d2a5c12045ce7a07d144a793b5b69
MahmoudElsayed <mahmood.m.elsaayed@gmail.com>
2025-01-01 18:24:30 +0200

    Refactor `StorageManager`
    - Renamed `REQUIRED_STORAGE_PROPERTIES` to `LOCALSTORAGE_USER_PROPERTIES`
    for clarity
    - Simplified the `getStorage` method's error-checking and implemented
    more graceful error handling logic

--- src/js/StorageManager.js
+++ src/js/StorageManager.js
@@ -1,16 +1,18 @@
 export default function StorageManager() {
-    const REQUIRED_STORAGE_PROPERTIES = ['tasks', 'projects']
+    const LOCALSTORAGE_USER_PROPERTIES = ['tasks', 'projects']
     // ensure that the necessary localStorage properties are initialized if user is new
-    for (let storageProperty of REQUIRED_STORAGE_PROPERTIES) {
+    for (let storageProperty of LOCALSTORAGE_USER_PROPERTIES) {
         if (!localStorage[storageProperty]) {
             localStorage[storageProperty] = '[]'
         }
     }
 
     function getStorage(storageKey) {
-        return localStorage[storageKey]
-            ? JSON.parse(localStorage[storageKey])
-            : null
+        if (!LOCALSTORAGE_USER_PROPERTIES.includes(storageKey)) {
+            console.error('Invalid storage key')
+            return null
+        }
+        return JSON.parse(localStorage[storageKey])
     }
 
     return { getStorage }

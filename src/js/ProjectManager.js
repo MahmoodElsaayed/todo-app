c2d1fd725d138bca42b95a5ee62947fc44eca56c
MahmoudElsayed <mahmood.m.elsaayed@gmail.com>
2025-01-03 16:59:19 +0200

    Add `addProject` method to `ProjectManager`

--- src/js/ProjectManager.js
+++ src/js/ProjectManager.js
@@ -1,4 +1,11 @@
 export default function ProjectManager(storageManager) {
     const STORAGE_KEY = 'projects'
     const projects = storageManager.getStorage(STORAGE_KEY)
+
+    function addProject(projectData) {
+        projects.push(projectData)
+        storageManager.setStorage(STORAGE_KEY, projects)
+    }
+
+    return { addProject }
 }

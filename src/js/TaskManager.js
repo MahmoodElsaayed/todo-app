9cca5d4317b42927dc35c714d56c1dce7e14a3ea
MahmoudElsayed <mahmood.m.elsaayed@gmail.com>
2025-01-03 15:05:44 +0200

    Add `generateTaskKey` and `Task` methods to `TaskManager`
    - Added the `generateTaskKey` method to standardize the generation of
    task access keys.
    - Added the `Task` factory to create uniform task objects, ensuring
    consistency across task management operations.

--- src/js/TaskManager.js
+++ src/js/TaskManager.js
@@ -1,4 +1,34 @@
 export default function TaskManager(storageManager, projectManager) {
     const STORAGE_KEY = 'tasks'
     const tasks = storageManager.getStorage(STORAGE_KEY)
+
+    function Task(taskData) {
+        const DEFAULT_VALUES = {
+            priority: 3,
+            isComplete: false,
+        }
+
+        return {
+            key: generateTaskKey(),
+            title: taskData.title,
+            description: taskData.description,
+            date: taskData.date,
+            priority: taskData.priority || DEFAULT_VALUES.priority,
+            isComplete: taskData.isComplete || DEFAULT_VALUES.isComplete,
+            project: projectManager.getActiveProject(),
+        }
+    }
+
+    function generateTaskKey() {
+        const validChars =
+            'abcdefghijklmnopqrstuvwsyzABCDEFGHIJKLMNOPQRSTUVWSYZ0123456789@#$&_'
+        const midPoint = Math.floor(validChars.length / 2)
+        return validChars
+            .split('')
+            .map((c) => ({ c, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map((obj) => obj.c)
+            .join('')
+            .slice(midPoint - 3, midPoint + 3)
+    }
 }

4bb08e832560a0557cf6857a26f9106a89acb710
MahmoudElsayed <mahmood.m.elsaayed@gmail.com>
2025-01-04 13:41:16 +0200

    Import and initialize the `model` layer modules

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,7 @@
 import StorageManager from './StorageManager'
+import ProjectManager from './ProjectManager'
 import TaskManager from './TaskManager'
+
+const storageManager = StorageManager()
+const projectManager = ProjectManager(storageManager)
+const taskManager = TaskManager(storageManager, projectManager)

006f010f59779479fccf7e9e64b69665efee7162
MahmoudElsayed <mahmood.m.elsaayed@gmail.com>
2025-01-04 13:20:06 +0200

    Add `filterTasksByProject` method to `ProjectManager`

--- src/js/ProjectManager.js
+++ src/js/ProjectManager.js
@@ -1,7 +1,9 @@
+import { startOfDay, differenceInDays } from 'date-fns'
+
 export default function ProjectManager(storageManager) {
     const STORAGE_KEY = 'projects'
     const projects = storageManager.getStorage(STORAGE_KEY)
-    let activeProject = ''
+    let activeProject = 'day' // Default startup value
 
     function addProject(projectTitle) {
         if (projects.includes(projectTitle)) {
@@ -34,11 +36,36 @@ export default function ProjectManager(storageManager) {
         return activeProject
     }
 
+    function filterTasksByProject() {
+        const TASKS_STORAGE_KEY = 'tasks'
+        const tasks = storageManager.getStorage(TASKS_STORAGE_KEY)
+
+        return tasks.reduce((accumulator, currentTask) => {
+            const todayDate = startOfDay(new Date())
+            const taskDate = startOfDay(new Date(currentTask.date))
+
+            const differenceBetweenDates = differenceInDays(taskDate, todayDate) // Returns:
+            // - Negative if taskDate is overdue (-<days diff>)
+            // - Positive if taskDate is upcoming (+<days diff>)
+            // - 0 if taskDate is today
+            if (
+                (activeProject === 'day' && differenceBetweenDates <= 0) ||
+                (activeProject === 'week' && differenceBetweenDates <= 7) ||
+                activeProject === currentTask.project ||
+                activeProject === 'all'
+            ) {
+                accumulator.push(currentTask)
+            }
+            return accumulator
+        }, [])
+    }
+
     return {
         addProject,
         deleteProject,
         getProjects,
         setActiveProject,
         getActiveProject,
+        filterTasksByProject,
     }
 }

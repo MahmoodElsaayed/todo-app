cb8e0f2c5e30ee74ef7de97a3b778f5f1fd05797
MahmoudElsayed <mahmood.m.elsaayed@gmail.com>
2025-01-07 13:46:47 +0200

    Add `extractFormData` method to `ScreenController`
    - Added method to process user input, ensuring it conforms to the
    standard data format used in the `Model` layer's modules and methods.

--- src/js/ScreenController.js
+++ src/js/ScreenController.js
@@ -2,6 +2,20 @@ export default function ScreenController(taskManager, projectManager) {
     const addTaskBtn = document.querySelector('#addTaskBtn')
     const closeModalBtns = document.querySelectorAll('.cancel.btn')
 
+    function extractFormData(target) {
+        const taskData = {}
+        const formFields = target.querySelectorAll(
+            ':is(input, select, textarea)'
+        )
+        formFields.forEach((field) => {
+            if (field.type === `checkbox`) {
+                field.value = field.checked
+            }
+            taskData[field.id] = field.value
+        })
+        return taskData
+    }
+
     // Open task modal
     addTaskBtn.addEventListener('click', () => {
         document.querySelector('#addTaskModal').showModal()

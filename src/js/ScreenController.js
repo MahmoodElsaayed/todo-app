f2b7e79f995dcbb9df22b75f199bb0f47080ecd0
MahmoudElsayed <mahmood.m.elsaayed@gmail.com>
2025-01-07 12:57:30 +0200

    Add `addTaskModal` with open and close functionality
    - Implemented a modal to gather user input for task creation.
    - Linked the open and close buttons to their respective JavaScript event
     listeners.

--- src/js/ScreenController.js
+++ src/js/ScreenController.js
@@ -1,1 +1,16 @@
-export default function ScreenController(taskManager, projectManager) {}
+export default function ScreenController(taskManager, projectManager) {
+    const addTaskBtn = document.querySelector('#addTaskBtn')
+    const closeModalBtns = document.querySelectorAll('.cancel.btn')
+
+    // Open task modal
+    addTaskBtn.addEventListener('click', () => {
+        document.querySelector('#addTaskModal').showModal()
+    })
+
+    // Close any open modal (without resetting)
+    closeModalBtns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            btn.closest('dialog').close()
+        })
+    })
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import StorageManager from './StorageManager'
 import ProjectManager from './ProjectManager'
 import TaskManager from './TaskManager'
+import ScreenController from './ScreenController'
 
 const storageManager = StorageManager()
 const projectManager = ProjectManager(storageManager)
 const taskManager = TaskManager(storageManager, projectManager)
+ScreenController(taskManager, projectManager)
